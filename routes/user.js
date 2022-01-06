346e328b461c67835b3db8ed4dd6d789a4a5836b
DucTran2310 <18520021@gm.uit.edu.vn>
2022-01-06 15:34:27 +0700

    Create models, routes, connect mongodb

---
+++ routes/user.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+
+// router.get("/usertest", (req, res) => {
+//   res.send("user test is successfully")
+// })
+
+// router.post("/userposttest", (req, res) => {
+//   const username = req.body.username;
+//   // console.log(username)
+//   res.send("Your username is: " + username)
+// })
+
+module.exports = router
