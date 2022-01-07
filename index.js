fdf4eb6d7f4f77a47b99a59b40a1601d854fd0be
DucTran2310 <18520021@gm.uit.edu.vn>
2022-01-07 14:56:17 +0700

    Create routes cart, order, product

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
-const express = require("express")
-const app = express()
-const mongoose = require("mongoose")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 
-const userRoute = require("./routes/user")
-const authRoute = require("./routes/auth")
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+
+const cors = require("cors");
 
 dotenv.config()
 
-mongoose.connect(
-  process.env.MONGO_URL
-).then(() => console.log("DB Connection Successfully"))
-  .catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
-// app.get("/api/test", () => {
-//   console.log("test is successfully!!!")
-// })
+app.use(cors());
 
-app.use(express.json())
-app.use("/api/auth", authRoute)
-app.use("/api/users", userRoute)
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log("I'm running !")
+  console.log("I'm running !");
 })
